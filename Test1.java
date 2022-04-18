ace8dd92c1207ab613bef184d284a7e10a12265d
Aradhana Singh <aradhana971127@gmail.com>
2022-04-18 12:51:27 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,6 +1,13 @@
 public class Test1 {
   public boolean equals(Object obj) {
     MyClass mc = (MyClass)obj; 
+    // int q = 10;
+    // int abcd =5;
+    
+    int i = 10;
+    for( ;i == 10;) {
+      return false;
+    }
     return true;
     
   }
